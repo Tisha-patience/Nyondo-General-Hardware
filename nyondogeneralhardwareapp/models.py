65492bb0f330caec004ff2acc673cc94c1452e90
Tisha Patience Malongo <tishapatiencemalongo@gmail.com>
2026-05-04 07:18:32 +0300

    created stock and supplier models,and templates to dynamically input data into the database for the stock and supplier forms

--- nyondogeneralhardwareapp/models.py
+++ nyondogeneralhardwareapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Supplier(models.Model):
+    supplier_name = models.CharField(max_length=100)                # Supplier name (e.g., Nile Cement Ltd.)
+    contact_number = models.CharField(max_length=15, blank=True)    # Contact phone number
+    address = models.TextField(blank=True)                 # Physical or mailing address
+    email = models.EmailField(blank=True)                  # Optional email contact
+    PAYMENT_CHOICES = [
+        ("Cash", "Cash"),
+        ("Credit", "Credit"),
+    ]
+    payment_terms = models.CharField(max_length=10, choices=PAYMENT_CHOICES, default="Cash")
+    credit_terms = models.CharField(max_length=50, blank=True, null=True)  # e.g., "30 days", "Cash on Delivery"
+    date_added = models.DateTimeField(auto_now_add=True) 
+
+
+class Stock(models.Model):
+    product_name = models.CharField(max_length=100)
+    specification = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    unit = models.CharField(max_length=20, blank=True)  # e.g. "bags"
+    unit_cost = models.DecimalField(max_digits=10, decimal_places=2)   # Buying price
+    unit_price =  models.DecimalField(max_digits=10, decimal_places=2)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    payment_mode = models.CharField(
+        max_length=10,
+        choices=[("Cash", "Cash"), ("Credit", "Credit")],
+        default="Cash"
+    )
+    date_received = models.DateField()
+
+  
--- nyondogeneralhardwareapp/views.py
+++ nyondogeneralhardwareapp/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from nyondogeneralhardwareapp.models import Stock, Supplier
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def login(request):
+    
     return render(request, 'login.html')
 
 def dashboard(request):
@@ -17,12 +19,32 @@ def sales(request):
     return render(request, 'sales.html')
 
 def supplier(request):
+    # Total suppliers
+    total_suppliers = Supplier.objects.count()
+
+    # Cash suppliers
+    cash_suppliers = Supplier.objects.filter(credit_terms="Cash").count()
+
+    # Credit suppliers (anything not Cash)
+    credit_suppliers = Supplier.objects.exclude(credit_terms="Cash").count()
+
+   
+    context = {
+        "total_suppliers": total_suppliers,
+        "cash_suppliers": cash_suppliers,
+        "credit_suppliers": credit_suppliers,
+        "suppliers": Supplier.objects.all(),
+        
+    }
+    return render(request, "supplier.html", context)
     return render(request, 'supplier.html')
 
 def stock(request):
-    return render(request, 'stock.html')
+    stocks = Stock.objects.all()
+    return render(request, 'stock.html', {"stocks" : stocks})
 
 def reports(request):
+
     return render(request, 'reports.html')
 
 
@@ -30,7 +52,27 @@ def customer_deposit(request):
     return render(request, 'customer-deposit.html')
 
 def supplier_reg(request):
-    return render(request, 'supplierReg.html')
+     if request.method == "POST":
+        payload = request.POST
+        # Supplier fields
+        sent_supplier_name = payload.get("supplier_name")
+        sent_contact_number = payload.get("contact_number")
+        sent_address = payload.get("address")
+        sent_email = payload.get("email")
+        sent_payment_terms = payload.get("payment_terms")
+        sent_credit_terms = payload.get("credit_terms") if sent_payment_terms == "Credit" else None
+
+        Supplier.objects.create(
+            supplier_name= sent_supplier_name,
+            contact_number= sent_contact_number,
+            address= sent_address,
+            email= sent_email,
+            payment_terms = sent_payment_terms,
+            credit_terms= sent_credit_terms,
+        )
+        return redirect ('accountssupplier')
+     print(request.POST)
+     return render(request, 'supplierReg.html')
 
 def back(request):
     return render(request, 'index.html')
@@ -48,7 +90,41 @@ def stock_edit(request):
     return render(request, 'stock-edit.html')
 
 def stock_reg(request):
-    return render(request, 'stock-reg.html')
+    if request.method == "POST":
+        payload = request.POST
+        sent_product_name = payload.get("product_name")
+        sent_specification = payload.get("specification")
+        sent_supplier = payload.get("supplier")
+        sent_payment_mode = payload.get("payment_mode")
+        sent_unit_cost = payload.get("unit_cost")
+        sent_unit_price = payload.get("unit_price")
+        sent_quantity = payload.get("quantity")
+        sent_unit = payload.get("unit")
+        sent_date_received = payload.get("date")
+
+        # ✅ fetch the Supplier object
+        supplier = Supplier.objects.get(id=sent_supplier)
+
+
+
+        Stock.objects.create(
+            product_name = sent_product_name,
+            specification= sent_specification,
+            supplier= supplier,
+            payment_mode= sent_payment_mode,
+            unit_cost= sent_unit_cost,
+            unit_price= sent_unit_price,
+            quantity= sent_quantity,
+            unit=sent_unit, 
+            date_received= sent_date_received,
+
+        )
+        return redirect ('accountsstock')
+
+
+    suppliers = Supplier.objects.all()
+
+    return render(request, 'stock-reg.html', {"suppliers": suppliers})
 
 def receipt_form(request):
     return render(request, 'receiptForm.html')
