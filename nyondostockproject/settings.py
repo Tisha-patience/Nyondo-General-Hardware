d30e8090d6d75903ef7a75517b1e0868972cec27
Tisha Patience Malongo <tishapatiencemalongo@gmail.com>
2026-06-08 14:22:50 +0300

    Fix Django version for deployment

--- nyondostockproject/settings.py
+++ nyondostockproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tk4#^5&mfv)9iyultjsd#(g3p!-gvo@y3x#4ionzfc_va76&+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['TishaPatienceMalongo.pythonanywhere.com']
 
 
 # Application definition
